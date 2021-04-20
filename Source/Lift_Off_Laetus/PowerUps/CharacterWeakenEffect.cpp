4dcac637423c6629ed227bb00eb906cd804166e4
MattJ <matt.d.johannesen@gmail.com>
2021-04-19 20:45:54 -0400

    Added all the other tile and character effects - ready to be filled in

---
+++ Source/Lift_Off_Laetus/PowerUps/CharacterWeakenEffect.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterWeakenEffect.h"
+
+void UCharacterWeakenEffect::ApplyEffect()
+{
+	//this->crewTarget->SetExtraMoveCost(1);
+}
+
+void UCharacterWeakenEffect::RemoveEffect()
+{
+	//this->crewTarget->SetExtraMoveCost(0);
+}
+
